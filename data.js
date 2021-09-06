e5b348797845aafc24a72bc65f9786a7a8910ee4
Tammy <tammycwong@gmail.com>
2021-09-05 21:05:54 -0400

    testing

---
+++ data.js
@@ -0,0 +1,61 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'ZZ plant',
+            category: 'Home plants',
+            image: '/images/zz.png',
+            price: 120,
+            countInStock: 4,
+            rating: 4.5,
+            numReviews: 1000,
+            description: 'indoor plant'
+        },
+        {
+            id: '2',
+            name: 'Money Tree',
+            category: 'Home plants',
+            image: '/images/moneytree.png',
+            price: 120,
+            countInStock: 0,
+            rating: 4,
+            numReviews: 208,
+            description: 'indoor plant'
+        },
+        {
+            id: '3',
+            name: 'Aloe Vera',
+            category: 'Home plants',
+            image: '/images/aloe.png',
+            price: 120,
+            countInStock: 63,
+            rating: 3.5,
+            numReviews: 453,
+            description: 'indoor plant'
+        },
+        {
+            id: '4',
+            name: 'Pothos',
+            category: 'Plants',
+            image: '/images/pothos.png',
+            price: 20,
+            countInStock: 21,
+            rating: 4.5,
+            numReviews: 1020,
+            description: 'indoor plant'
+        },
+        {
+            id: '5',
+            name: 'Snake Plant',
+            category: 'Plants',
+            image: '/images/snakeplant.png',
+            price: 80,
+            countInStock: 87,
+            rating: 5,
+            numReviews: 2560,
+            description: 'indoor plant'
+        },
+    ],
+};
+
+export default data;
