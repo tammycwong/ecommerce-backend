1a200c423e3c087f6de842e34a250fcb0172db0c
Tammy <tammycwong@gmail.com>
2021-09-07 12:40:45 -0400

    changed inventory in backend

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const data = {
             category: 'Home plants',
             image: '/images/zz.png',
             price: 120,
-            countInStock: 4,
+            countInStock: 20,
             rating: 4.5,
             numReviews: 1000,
             description: 'indoor plant'
@@ -28,7 +28,7 @@ const data = {
             category: 'Home plants',
             image: '/images/aloe.png',
             price: 120,
-            countInStock: 63,
+            countInStock: 20,
             rating: 3.5,
             numReviews: 453,
             description: 'indoor plant'
@@ -39,7 +39,7 @@ const data = {
             category: 'Plants',
             image: '/images/pothos.png',
             price: 20,
-            countInStock: 21,
+            countInStock: 0,
             rating: 4.5,
             numReviews: 1020,
             description: 'indoor plant'
@@ -50,7 +50,7 @@ const data = {
             category: 'Plants',
             image: '/images/snakeplant.png',
             price: 80,
-            countInStock: 87,
+            countInStock: 20,
             rating: 5,
             numReviews: 2560,
             description: 'indoor plant'
