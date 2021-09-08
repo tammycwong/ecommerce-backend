e767411939a8549453c568f4031af426a32db5e5
Tammy <tammycwong@gmail.com>
2021-09-07 21:42:59 -0400

    need to fix error in user model

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
+
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/plantsy', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
+app.use('/api/users', userRouter);
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x.id === req.params.id);
     if (product) {
@@ -15,10 +24,15 @@ app.get('/api/products/:id', (req, res) => {
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
-
+app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+})
+//error catcher from asyc handler in userrouter
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
